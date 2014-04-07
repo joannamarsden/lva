48dc0b1abfc31ebccf130df43bf975921bf4268a
Joanna Marsden <joanna.marsden@gmail.com>
2014-04-06 23:41:22 -0400

    initial commit, refinery, zurb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lva::Application.config.secret_token = '39ad6769f60b63168af37c9cec6909069f23cb9a388076fb0c57576b10b81fa427e57f65c6a8fe23f994b540a8001b6d2cd01b2125faebb109f6f6f330251131'
