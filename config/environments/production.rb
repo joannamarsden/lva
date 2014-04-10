fa84a931cf28c466937da4608f8893bce5ef1b98
Joanna Marsden <joanna.marsden@gmail.com>
2014-04-10 18:53:03 -0400

    *(^&^(%(

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Lva::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = true
 
   # Disable delivery errors, bad email addresses will be ignored
   if config.respond_to?(:action_mailer)
