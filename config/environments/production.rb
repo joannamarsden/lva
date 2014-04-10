0ab5fe1f84e1cf8a4dadb1f7f3cf264b3b683c7b
Joanna Marsden <joanna.marsden@gmail.com>
2014-04-10 18:44:43 -0400

    preparing for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,8 @@ Lva::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  config.assets.initialize_on_precompile = true
+
   # Disable delivery errors, bad email addresses will be ignored
   if config.respond_to?(:action_mailer)
     # config.action_mailer.raise_delivery_errors = false
